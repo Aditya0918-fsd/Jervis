cc7142b0f9b758f97417ca2a108d09a51aacaaae
Your GitHub Username <your.github.email@example.com>
2026-01-23 00:34:08 +0530

    Activating the all the systems

--- engine/db.py
+++ engine/db.py
@@ -1,19 +1,18 @@
 import sqlite3
 
 conn = sqlite3.connect('shree.db')
-
 cursor = conn.cursor()
 
-query = "   CREATE TABLE IF NOT EXISTS sys_command (id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
-cursor.execute(query)
-
-query = "INSERT INTO sys_command VALUES (null, 'VSCode', 'C:\\Users\\Admin\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe')"
-cursor.execute(query)
-conn.commit()
-
-# query = "   CREATE TABLE IF NOT EXISTS web_command (id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+# query = "   CREATE TABLE IF NOT EXISTS sys_command (id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
 # cursor.execute(query)
 
-# query = "INSERT INTO web_command VALUES (null, 'phpMyAdmin', 'http://localhost/phpmyadmin/')"
+# query = "INSERT INTO sys_command VALUES (null, 'vs code', 'C:\\Users\\Admin\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe')"
 # cursor.execute(query)
 # conn.commit()
+
+query = "   CREATE TABLE IF NOT EXISTS web_command (id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+cursor.execute(query)
+
+query = "INSERT INTO web_command VALUES (10, 'php my admin', 'http://localhost/phpmyadmin/')"
+cursor.execute(query)
+conn.commit()
--- engine/features.py
+++ engine/features.py
@@ -1,12 +1,17 @@
 import os
 import re
+import webbrowser
 from playsound import playsound
 import eel
+import sqlite3
 from engine.command import speak
 from engine.config import ASSISTANT_NAME
 import pywhatkit as kit
 
 #Playing assistent sound function
+
+conn = sqlite3.connect('shree.db')
+cursor = conn.cursor()
 
 @eel.expose
 def playAssistantSound():
@@ -19,11 +24,36 @@ def openCommand(query):
     query = query.replace("open", "")
     query.lower()
 
-    if query!="":
-        speak("Opening " + query)
-        os.system('start ' + query)
-    else:
-        speak("Not found")
+    app_name = query.strip()
+
+    if app_name != "":
+
+        try:
+            cursor.execute(
+                'SELECT path FROM sys_command WHERE name IN (?)', (app_name,))
+            result = cursor.fetchall()
+
+            if len(result) != 0:
+                speak("Opening " +query)
+                os.startfile(result[0][0])
+
+            elif len(result) == 0:
+                cursor.execute(
+                'SELECT url FROM web_command WHERE name IN (?)', (app_name,))
+                result = cursor.fetchall()
+
+                if len(result) != 0:
+                    speak("Opening " +query)
+                    webbrowser.open(result[0][0])
+
+                else:
+                    speak("Opening "+query)
+                    try:
+                        os.system('Start '+query)
+                    except:
+                        speak("Not found")
+        except:
+            speak("Some thing wents wrong")
 
 def PlayYoutube(query):
     search_term = extract_yt_term(query)
