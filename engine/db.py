15a9643f2f08ac9effa909b93efe7c85c78cf7ec
Your GitHub Username <your.github.email@example.com>
2026-01-24 01:43:58 +0530

    Adding whatsApp numbers in the database.

--- engine/db.py
+++ engine/db.py
@@ -1,7 +1,8 @@
+import csv
 import sqlite3
 
-conn = sqlite3.connect('shree.db')
-cursor = conn.cursor()
+con = sqlite3.connect('shree.db')
+cursor = con.cursor()
 
 # query = "   CREATE TABLE IF NOT EXISTS sys_command (id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
 # cursor.execute(query)
@@ -10,9 +11,47 @@ cursor = conn.cursor()
 # cursor.execute(query)
 # conn.commit()
 
-query = "   CREATE TABLE IF NOT EXISTS web_command (id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
-cursor.execute(query)
+# #Testing modeles
+# app_name = "android studio"
+# cursor.execute("SELECT path FROM sys_command WHERE name IN (?)", (app_name,))
+# result = cursor.fetchone()
+# print(result[0][0])
 
-query = "INSERT INTO web_command VALUES (10, 'php my admin', 'http://localhost/phpmyadmin/')"
-cursor.execute(query)
-conn.commit()
+# query = "   CREATE TABLE IF NOT EXISTS web_command (id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (10, 'php my admin', 'http://localhost/phpmyadmin/')"
+# cursor.execute(query)
+# conn.commit()
+
+# # Create a table with the desired columns
+# cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
+
+
+# # Specify the column indices you want to import (0-based index)
+# # Example: Importing the 1st and 3rd columns
+# desired_columns_indices = [0, 4]
+# # Read data from CSV and insert into SQLite table for the desired columns
+# with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
+#     csvreader = csv.reader(csvfile)
+#     for row in csvreader:
+#         selected_data = [row[i] for i in desired_columns_indices]
+#         cursor.execute(''' INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
+
+# # Commit changes and close connection
+# con.commit()
+# con.close()
+
+# # Single Contact Insert 
+# query = "INSERT INTO contacts VALUES (null,'pawan', '1234567890')"
+# cursor.execute(query)
+# con.commit()
+
+# # Search Contacts from database
+# query = 'kunal'
+# query = query.strip().lower()
+
+# cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
+# results = cursor.fetchall()
+# print(results[0][0])
+
